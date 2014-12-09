9c51c5f74c9b92e02342dec6e94e5f0791d01398
DiegoZTC <liberatux@gmail.com>
2014-12-09 17:16:45 -0500

    Splash Screen Add

--- winthelottery/src/main/java/com/amazon/winthelottery/SplashActivity.java
+++ winthelottery/src/main/java/com/amazon/winthelottery/SplashActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends Activity {
-
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
 
