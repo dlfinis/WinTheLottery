adadc39e26c4b94fe0b8696f09496b0a3b0bd6c8
DiegoZTC <liberatux@gmail.com>
2014-12-15 16:48:14 -0500

    Edit Activity Main

--- winthelottery/src/main/java/com/amazon/winthelottery/MainActivity.java
+++ winthelottery/src/main/java/com/amazon/winthelottery/MainActivity.java
@@ -4,16 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+    Button button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.btnSettings);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(MainActivity.this,
+                        "Button Lucky (selector) is clicked!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
+
+
+    public void MessageTestView (View v)
+    {
+
+        Toast.makeText(MainActivity.this,
+                "Button Lucky (selector) is clicked!",
+                Toast.LENGTH_SHORT).show();
+
     }
 
 
